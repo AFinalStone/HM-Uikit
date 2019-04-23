e57fbf1bc8de9575f1f1ebd9e61a9195a61d369f
houjinyun <houjinyun@gmail.com>
2019-04-23 19:14:48 +0800

    圆角图片版本回退

--- library/src/main/java/com/hm/iou/uikit/ShapedImageView.java
+++ library/src/main/java/com/hm/iou/uikit/ShapedImageView.java
@@ -11,29 +11,29 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.graphics.Xfermode;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
+import android.os.Build;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
 import java.lang.ref.WeakReference;
+import java.util.Arrays;
 
 /**
  * 可以实现圆角和圆角矩形的ImageView
  * SHI
  * 2016年5月12日 19:39:16
  */
-public class ShapedImageView extends android.support.v7.widget.AppCompatImageView {
+public class ShapedImageView extends AppCompatImageView {
 
     private static final int SHAPE_MODE_ROUND_RECT = 1;
     private static final int SHAPE_MODE_CIRCLE = 2;
 
-    //模型 1 矩形， 2 圆角
     private int mShapeMode = 0;
-    //边角半径
-    private float mBorderRadius;
-    private WeakReference<Bitmap> mWeakBitmap;
-    private Xfermode mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
-    private Bitmap mMaskBitmap;
+    private float mRadius = 0;
+    private Shape mShape;
     private Paint mPaint;
-
 
     public ShapedImageView(Context context) {
         super(context);
@@ -51,111 +51,58 @@ public class ShapedImageView extends android.support.v7.widget.AppCompatImageVie
     }
 
     private void init(AttributeSet attrs) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setLayerType(LAYER_TYPE_HARDWARE, null);
+        }
         if (attrs != null) {
             TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.HmShapedImageView);
             mShapeMode = a.getInt(R.styleable.HmShapedImageView_shape_mode, 0);
-            mBorderRadius = a.getDimension(R.styleable.HmShapedImageView_round_radius, SHAPE_MODE_CIRCLE);
+            mRadius = a.getDimension(R.styleable.HmShapedImageView_round_radius, 0);
             a.recycle();
         }
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
-        setLayerType(LAYER_TYPE_HARDWARE, mPaint);
+        mPaint.setFilterBitmap(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        /**
-         * 如果类型是圆形，则强制改变view的宽高一致，以小值为准
-         */
-        if (mShapeMode == SHAPE_MODE_CIRCLE) {
-            int min = Math.min(getMeasuredWidth(), getMeasuredHeight());
-            setMeasuredDimension(min, min);
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        if (changed) {
+            switch (mShapeMode) {
+                case SHAPE_MODE_ROUND_RECT:
+                    break;
+                case SHAPE_MODE_CIRCLE:
+                    int min = Math.min(getWidth(), getHeight());
+                    mRadius = (float) min / 2;
+                    break;
+            }
+            if (mShape == null) {
+                float[] radius = new float[8];
+                Arrays.fill(radius, mRadius);
+                mShape = new RoundRectShape(radius, null, null);
+            }
+            mShape.resize(getWidth(), getHeight());
         }
-
     }
-
 
     @Override
     protected void onDraw(Canvas canvas) {
-        //在缓存中取出bitmap
-        Bitmap bitmap = mWeakBitmap == null ? null : mWeakBitmap.get();
-
-        if (null == bitmap || bitmap.isRecycled()) {
-            //拿到Drawable
-            Drawable drawable = getDrawable();
-            if (drawable != null) {
-                /**
-                 * 把默认的src图片资源作为画布来进行缩放绘制
-                 */
-                //获取drawable的宽和高
-                int dWidth = drawable.getIntrinsicWidth();
-                int dHeight = drawable.getIntrinsicHeight();
-                //创建bitmap
-                bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-                float scale = 1.0f;
-                //创建画布
-                Canvas drawCanvas = new Canvas(bitmap);
-                //按照bitmap的宽高，以及view的宽高，计算缩放比例；因为设置的src宽高比例可能和imageview的宽高比例不同，这里我们不希望图片失真；
-                if (mShapeMode == SHAPE_MODE_ROUND_RECT) {
-                    // 如果图片的宽或者高与view的宽高不匹配，计算出需要缩放的比例；缩放后的图片的宽高，一定要大于我们view的宽高；所以我们这里取大值；
-                    scale = Math.max(getWidth() * 1.0f / dWidth, getHeight()
-                            * 1.0f / dHeight);
-                } else {
-                    scale = getWidth() * 1.0F / Math.min(dWidth, dHeight);
+        int saveCount = canvas.getSaveCount();
+        canvas.save();
+        super.onDraw(canvas);
+        switch (mShapeMode) {
+            case SHAPE_MODE_ROUND_RECT:
+            case SHAPE_MODE_CIRCLE:
+                if (mShape != null) {
+                    mShape.draw(canvas, mPaint);
                 }
-                //根据缩放比例，设置bounds，相当于缩放图片了
-                drawable.setBounds(0, 0, (int) (scale * dWidth),
-                        (int) (scale * dHeight));
-                drawable.draw(drawCanvas);
-
-                /**
-                 * 获取需要覆盖在src上面的遮罩bitmap，并设置画笔为PorterDuff.Mode.DST_IN模式，来进行掏空绘制，把周边给覆盖掉，只保留中间
-                 */
-                if (mMaskBitmap == null || mMaskBitmap.isRecycled()) {
-                    mMaskBitmap = getMaskBitmap();
-                }
-                // Draw Bitmap.
-                mPaint.reset();
-                mPaint.setFilterBitmap(false);
-                mPaint.setXfermode(mXfermode);
-                //绘制形状
-                drawCanvas.drawBitmap(mMaskBitmap, 0, 0, mPaint);
-                mPaint.setXfermode(null);
-                //将准备好的bitmap绘制出来
-                canvas.drawBitmap(bitmap, 0, 0, null);
-                //bitmap缓存起来，避免每次调用onDraw，分配内存
-                mWeakBitmap = new WeakReference<Bitmap>(bitmap);
-            }
+                break;
         }
-        //如果bitmap还存在，则直接绘制即可
-        if (bitmap != null) {
-            mPaint.setXfermode(null);
-            canvas.drawBitmap(bitmap, 0.0f, 0.0f, mPaint);
-            return;
-        }
-
-    }
-
-    /**
-     * 绘制遮罩的Bitmap形状
-     *
-     * @return
-     */
-    public Bitmap getMaskBitmap() {
-        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setColor(Color.BLACK);
-
-        if (mShapeMode == SHAPE_MODE_ROUND_RECT) {
-            canvas.drawRoundRect(new RectF(0, 0, getWidth(), getHeight()), mBorderRadius, mBorderRadius, paint);
-        } else {
-            int min = Math.min(getWidth(), getHeight());
-            float radius = (float) min / 2;
-            canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, paint);
-        }
-        return bitmap;
+        canvas.restoreToCount(saveCount);
     }
 
 }
+
